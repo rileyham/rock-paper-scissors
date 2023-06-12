93a665361e7117d2b6095eb2853aeeb472092fb3
rileyham <rileyham21@gmail.com>
2023-06-11 22:06:05 -0400

    Add getComputerChoice and playRound functions

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("Hello World");
+// gitComputerChoice - randomly return rock, paper, or scissors
+function getComputerChoice() {
+    let result = Math.random() * 3;
+    let resultString;
+
+    if (result<1) {
+        resultString = "Rock";
+    } 
+    else if (result<2 && result>=1) {
+        resultString = "Paper";
+    }
+    else {
+        resultString = "Scissors";
+    }
+    return resultString;
+}
+
+// playRound take in player and computer selection 
+// and return a string announcing the result
+function playRound(playerChoice,computerChoice) {
+    let outputString;
+    if(playerChoice =="Rock") {
+        if(computerChoice == "Rock") {
+            outputString = "You Tied! You both played rock.";
+        }
+        else if(computerChoice == "Scissors") {
+            outputString = "You Win! Rock beats scissors.";
+        }
+        else {
+            outputString = "You Lose! Paper beats rock.";
+        }
+    }
+    else if(playerChoice == "Scissors") {
+        if(computerChoice == "Rock") {
+            outputString = "You Lose! Rock beats scissors.";
+        }
+        else if(computerChoice == "Scissors") {
+            outputString = "You Tied! You both played scissors.";
+        }
+        else {
+            outputString = "You Win! Scissors beats paper.";            
+        }
+    }
+    else {
+        if(computerChoice == "Rock") {
+            outputString = "You Win! Paper beats rock.";
+        }
+        else if(computerChoice == "Scissors") {
+            outputString = "You Lose! Scissors beats paper.";
+        }
+        else {
+            outputString = "You Tied! You both played paper.";            
+        }
+    }
+    return outputString;
+}
